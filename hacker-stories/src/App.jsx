35a412327cce765d04dd133ca563decd0fbaa329
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-06-06 14:29:14 -0400

    stories are no longer global, split out Item to its own component

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -8,54 +8,23 @@ if(import.meta.hot) {
   })
 }
 
-const listStock = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const listMine = [
-  {
-    title: "Another book",
-    url: "https://www.example.com",
-    author: "ME",
-    num_comments: 0,
-    points: 0,
-    objectID: 2,
-  },
-]
+const Item = ({item}) => (
+  <ul>
+    <li>Title: <a href={item.url}>{item.title}</a></li>
+    <li>Author: {item.author}</li>
+    <li>Comments: {item.num_comments}</li>
+    <li>Points: {item.points}</li>
+  </ul>
+)
 
 const List = ({name, listParam}) => (
   <div>
-  <h3>{name}:</h3>
-  {listParam.map(item =>
-      <div>
-        <div key={item.objectID}>
-        <ul>
-          <li>Title: <a href={item.url}>{item.title}</a></li>
-          <li>Author: {item.author}</li>
-          <li>Comments: {item.num_comments}</li>
-          <li>Points: {item.points}</li>
-        </ul>
-      </div>
-    </div>
-  )}
+    <h3>{name}:</h3>
+    {listParam.map(item =>
+      <Item key={item.objectID} item={item} />
+    )}
   </div>
 )
-
 
 const Search = () => {
   const handleChange = (event) => {
@@ -74,13 +43,44 @@ const Search = () => {
   );
 }
 
-const App = () => (
-  <div>
-    <h1>{welcome.greeting} {welcome.title}!</h1>
-    <Search />
-    <List name="Stock list" listParam={listStock}/>
-    <List name="My List" listParam={listMine}/>
-  </div>
-)
+const App = () => {
+  const storiesStock = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  
+  const storiesMine = [
+    {
+      title: "Another book",
+      url: "https://www.example.com",
+      author: "ME",
+      num_comments: 0,
+      points: 0,
+      objectID: 2,
+    },
+  ]
+  return (
+    <div>
+      <h1>{welcome.greeting} {welcome.title}!</h1>
+      <Search />
+      <List name="Stock list" listParam={storiesStock}/>
+      <List name="My List" listParam={storiesMine}/>
+    </div>
+  )
+}
 
 export default App;
