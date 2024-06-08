c8f8d3d422b72cb5fc7d488c29f1c40d307e0103
Jason Viers <2023094+Beanalby@users.noreply.github.com>
2024-06-08 13:59:33 -0400

    Added Delete functionality

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,13 +1,9 @@
 import * as React from 'react';
+if(import.meta.hot) { import.meta.hot.dispose(() => { console.clear(); })}
 
 const welcome = {
   title: "ReactObject",
   greeting: "Hello,",
-}
-if(import.meta.hot) {
-  import.meta.hot.dispose(() => {
-    console.clear();
-  })
 }
 
 const useLocalStorage = (storageKey, fallbackState) => {
@@ -21,20 +17,23 @@ const useLocalStorage = (storageKey, fallbackState) => {
   return [value, setValue];
 }
 
-const Item = ({item}) => (
-  <ul>
-    <li>Title: <a href={item.url}>{item.title}</a></li>
-    <li>Author: {item.author}</li>
-    <li>Comments: {item.num_comments}</li>
-    <li>Points: {item.points}</li>
-  </ul>
+const Item = ({item, onDelete}) => (
+  <div style={{clear: "both"}}>
+    <button style={{marginTop: "1.2em", float: "left"}} onClick={() => onDelete(item)}>Delete</button>
+    <ul style={{float: "left"}}>
+      <li>Title: <a href={item.url}>{item.title}</a></li>
+      <li>Author: {item.author}</li>
+      <li>Comments: {item.num_comments}</li>
+      <li>Points: {item.points}</li>
+    </ul>
+  </div>
 )
 
-const List = ({name, list}) => (
+const List = ({name, list, onDelete}) => (
   <div>
     <h3>{name}:</h3>
     {list.map(item =>
-      <Item key={item.objectID} item={item} />
+      <Item key={item.objectID} item={item} onDelete={onDelete}/>
     )}
   </div>
 )
@@ -51,7 +50,7 @@ const App = () => {
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
-  const stories = [
+  const initStories = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -69,6 +68,10 @@ const App = () => {
       objectID: 1,
     },
   ];
+  const [stories, setStories] = React.useState(initStories);
+  const handleDelete = (story) => {
+    setStories(stories.filter((s) => s.objectID != story.objectID));
+  };
   const storiesSearched = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase()));
   // const storiesMine = [
@@ -89,7 +92,7 @@ const App = () => {
         value={searchTerm} onInputChange={handleSearch}>
           <strong>Search</strong>:
       </InputWithLabel>
-      <List name="Stock list" list={storiesSearched}/>
+      <List name="Stock list" list={storiesSearched} onDelete={handleDelete}/>
       {/* <List name="My List" listParam={storiesMine}/> */}
     </div>
   )
